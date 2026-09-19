d62859613f40efee55b53d997ec1a42519357b68
timmashkov <bodomizer@yandex.ru>
2026-09-19 12:25:19 +0300

    init_project

---
+++ src/application/providers/server_provider.py
@@ -0,0 +1,16 @@
+from dishka import Provider, Scope, provide
+
+from application.config import Settings
+from application.server import APIServer
+
+
+class ServerProvider(Provider):
+    scope = Scope.APP
+
+    @provide(scope=Scope.APP)
+    def provide_api_server(self, settings: Settings) -> APIServer:
+        """Создаёт экземпляр APIServer с инжектированными settings."""
+        return APIServer(
+            settings=settings,
+            routers=[],
+        )
---
+++ src/application/providers/use_cases_provider.py
@@ -0,0 +1,10 @@
+from dishka import Provider, Scope, provide
+
+from infrastructure.database import UnitOfWork
+
+
+#class UseCaseProvider(Provider):
+
+   # @provide(scope=Scope.REQUEST)
+    #def provide_perm_use_cases(self, unit_of_work: UnitOfWork):
+        #pass
